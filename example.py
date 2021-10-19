62f5e58c859f4eb852dbf32ad58e07da970cbd99
Ashish <68690233+asheeeshh@users.noreply.github.com>
2021-10-19 21:57:35 +0530

    Update example.py

--- example.py
+++ example.py
@@ -2,5 +2,16 @@ import weeby
 
 my_weeby = weeby.Weeby('')
 
-print(my_weeby.get_json_response().stats())
+
+my_weeby.get_json_response().random(type="") #Returns Random Response -> type (8ball, belikebill, dadjoke, geography, joke, roast)
+
+my_weeby.get_json_response().animal_image(type="") #Returns Random Animal Image Links -> type (bird, bunny, cat, dog, fox, goose, kangaroo, koala, lizard, panda)
+
+my_weeby.get_json_response().meme(type="") #Returns Random Meme -> type (meme, memes, wholesome, dank)
+
+my_weeby.get_json_response().word(type="") #Returns Random Word or List of Words -> type (random, halloween, christmas, list)
+
+my_weeby.get_json_response().lyrics(track="") #Returns Lyrics -> track = track name with or without artist name
+
+my_weeby.get_json_response().stats() #Returns WeebyAPI Stats
 
